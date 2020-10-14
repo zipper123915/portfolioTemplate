40ace4c5ffec4db4f48439ef5969df750d8dcf70
zipper <zippper123915@gmail.com>
2020-10-14 08:14:09 +0300

    Finish rewrite

---
+++ src/blocks/intro/intro.js
@@ -0,0 +1,7 @@
+import Glide from '@glidejs/glide/dist/glide.modular.esm';
+
+const glide = new Glide('.intro__slider');
+
+glide.mount();
+
+export default glide;
--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,5 @@ import '../../.wfLayout/webpCheck';
 import '../blocks/hero/hero';
 import '../blocks/header/header';
 import '../blocks/portfolio/portfolio';
+import '../blocks/services/service';
+import '../blocks/intro/intro';
